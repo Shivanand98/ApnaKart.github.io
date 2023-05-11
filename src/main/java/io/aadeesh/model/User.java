bd8aa1cc818920d216c615cae4c32efff68ab662
Shiva <shivanandbiradar7799@gmail.com>
2023-05-11 19:21:22 +0530

    first commit

---
+++ src/main/java/io/aadeesh/model/User.java
@@ -0,0 +1,69 @@
+package io.aadeesh.model;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer user_id;
+	
+	private String user_name;
+	private String user_mobile;
+	private String user_email;
+	private String user_pass;
+	private String confirmpassword;
+	
+	public Integer getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+	
+	
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_mobile() {
+		return user_mobile;
+	}
+	public void setUser_mobile(String user_mobile) {
+		this.user_mobile = user_mobile;
+	}
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public String getUser_pass() {
+		return user_pass;
+	}
+	public void setUser_pass(String user_pass) {
+		this.user_pass = user_pass;
+	}
+	
+	public String getConfirmpassword() {
+		return confirmpassword;
+	}
+	public void setConfirmpassword(String confirmpassword) {
+		this.confirmpassword = confirmpassword;
+	}
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", user_name=" + user_name + ", user_mobile=" + user_mobile
+				+ ", user_email=" + user_email + ", user_pass=" + user_pass + ", confirmpassword=" + confirmpassword
+				+ "]";
+	}
+	
+	
+}
